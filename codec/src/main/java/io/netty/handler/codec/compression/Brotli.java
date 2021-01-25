cbd0788a671c642fa9aaaa6d1cd715edf9ff06fe
Stephane Landelle <slandelle@gatling.io>
2021-01-25 21:56:16 +0100

    Introduce BrotliDecoder
    Motivation:
    
    Netty lacks client side support for decompressing Brotli compressed response bodies.
    
    Modification:
    
    * Introduce optional dependency to brotli4j by @hyperxpro. It will be up to the user to provide the brotli4j libraries for the target platform in the classpath. brotli4j is currently available for Linux, OSX and Windows, all for x86 only.
    * Introduce BrotliDecoder in codec module
    * Plug it onto `HttpContentDecompressor` for HTTP/1 and `DelegatingDecompressorFrameListener` for HTTP/2
    * Add test in `HttpContentDecoderTest`
    
    Result:
    
    Netty now support decompressing Brotli compressed response bodies.

---
+++ codec/src/main/java/io/netty/handler/codec/compression/Brotli.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.compression;
+
+import com.aayushatharva.brotli4j.Brotli4jLoader;
+import io.netty.util.internal.PlatformDependent;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+
+public final class Brotli {
+
+    private static final InternalLogger logger = InternalLoggerFactory.getInstance(Brotli.class);
+    private static final ClassNotFoundException CNFE;
+
+    static {
+        ClassNotFoundException cnfe = null;
+
+        try {
+            Class.forName("com.aayushatharva.brotli4j.Brotli4jLoader", false,
+                PlatformDependent.getClassLoader(Brotli.class));
+        } catch (ClassNotFoundException t) {
+            cnfe = t;
+            logger.debug(
+                "brotli4j not in the classpath; Brotli support will be unavailable.");
+        }
+
+        CNFE = cnfe;
+
+        // If in the classpath, try to load the native library and initialize brotli4j.
+        if (cnfe == null) {
+            Throwable cause = Brotli4jLoader.getUnavailabilityCause();
+            if (cause != null) {
+                logger.debug("Failed to load brotli4j; Brotli support will be unavailable.", cause);
+            }
+        }
+    }
+
+    public static boolean isAvailable() {
+        return CNFE == null && Brotli4jLoader.isAvailable();
+    }
+
+    public static void ensureAvailability() throws Throwable {
+        if (CNFE != null) {
+            throw CNFE;
+        }
+        Brotli4jLoader.ensureAvailability();
+    }
+
+    private Brotli() {
+    }
+}
---
+++ codec/src/main/java/io/netty/handler/codec/compression/BrotliDecoder.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2021 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.compression;
+
+import com.aayushatharva.brotli4j.decoder.DecoderJNI;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public final class BrotliDecoder extends ByteToMessageDecoder {
+
+    private enum State {
+        DONE, NEEDS_MORE_INPUT, ERROR
+    }
+
+    private DecoderJNI.Wrapper decoder;
+    private final AtomicBoolean destroyed = new AtomicBoolean();
+
+    static {
+        try {
+            Brotli.ensureAvailability();
+        } catch (Throwable throwable) {
+            throw new ExceptionInInitializerError(throwable);
+        }
+    }
+
+    private State decompress(ByteBuf input, List<Object> output) {
+        for (;;) {
+            switch (decoder.getStatus()) {
+                case DONE:
+                    return State.DONE;
+
+                case OK:
+                    decoder.push(0);
+                    break;
+
+                case NEEDS_MORE_INPUT:
+                    if (decoder.hasOutput()) {
+                        output.add(Unpooled.wrappedBuffer(decoder.pull()));
+                    }
+
+                    if (!input.isReadable()) {
+                        return State.NEEDS_MORE_INPUT;
+                    }
+
+                    ByteBuffer decoderInputBuffer = decoder.getInputBuffer();
+                    decoderInputBuffer.clear();
+                    int readBytes = readBytes(input, decoderInputBuffer);
+                    decoder.push(readBytes);
+                    break;
+
+                case NEEDS_MORE_OUTPUT:
+                    output.add(Unpooled.wrappedBuffer(decoder.pull()));
+                    break;
+
+                default:
+                    return State.ERROR;
+            }
+        }
+    }
+
+    private static int readBytes(ByteBuf in, ByteBuffer dest) {
+        int limit = Math.min(in.readableBytes(), dest.remaining());
+        ByteBuffer slice = dest.slice();
+        slice.limit(limit);
+        in.readBytes(slice);
+        dest.position(dest.position() + limit);
+        return limit;
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        decoder = new DecoderJNI.Wrapper(8 * 1024);
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        if (!in.isReadable()) {
+            return;
+        }
+
+        try {
+            State state = decompress(in, out);
+            if (state == State.DONE) {
+                destroy();
+            } else if (state == State.ERROR) {
+                throw new DecompressionException("Brotli stream corrupted");
+            }
+        } catch (Exception e) {
+            destroy();
+            throw e;
+        }
+    }
+
+    private void destroy() {
+        if (destroyed.compareAndSet(false, true)) {
+            decoder.destroy();
+        }
+    }
+
+    @Override
+    protected void handlerRemoved0(ChannelHandlerContext ctx) throws Exception {
+        try {
+            destroy();
+        } finally {
+            super.handlerRemoved0(ctx);
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        try {
+            destroy();
+        } finally {
+            super.channelInactive(ctx);
+        }
+    }
+}
